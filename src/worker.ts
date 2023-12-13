7b0e754b836e8c992869482274743cfe9c4bee27
sinchang <sinchangwen@gmail.com>
2023-12-13 17:16:24 +0800

    Add cache

--- src/worker.ts
+++ src/worker.ts
@@ -4,16 +4,27 @@ import { ImageResponse } from '@skorfmann/workers-og';
 export interface Env {}
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
 		const cover = new URL(request.url).searchParams.get('cover');
 		if (!cover) {
 			return new Response('Missing cover', { status: 400 });
 		}
+		const cache = caches.default;
+		const cacheKey = cover;
+		let response = await cache.match(cacheKey);
 
-		return new ImageResponse(template(cover), {
-			format: 'png',
-			width: 1200,
-			height: 630,
-		});
+		if (!response) {
+			const response = new ImageResponse(template(cover), {
+				format: 'png',
+				width: 1200,
+				height: 630,
+			});
+
+			response.headers.append('Cache-Control', 's-maxage=604800');
+
+			ctx.waitUntil(cache.put(cacheKey, response.clone()));
+		}
+
+		return response;
 	},
 };
