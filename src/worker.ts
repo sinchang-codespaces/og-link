2819fe0e6358aa5cf41afce096610b7ae092c028
sinchang <sinchangwen@gmail.com>
2023-12-13 17:09:21 +0800

    Add cover og

--- src/worker.ts
+++ src/worker.ts
@@ -1,46 +1,19 @@
-import template from "./template";
-import { ImageResponse } from "@skorfmann/workers-og";
+import template from './template';
+import { ImageResponse } from '@skorfmann/workers-og';
 
-export interface Env {
-}
-
-// see https://en.wikipedia.org/wiki/Base64#URL_applications
-function decodeBase64Url(base64Url: string) {
-  // Replace URL specific chars
-  base64Url = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-
-  // Add padding
-  while (base64Url.length % 4) {
-    base64Url += '=';
-  }
-
-  const binStr = atob(base64Url);
-  const binStrLen = binStr.length;
-  const bytes = new Uint8Array(binStrLen);
-
-  for (let i = 0; i < binStrLen; i++) {
-    bytes[i] = binStr.charCodeAt(i);
-  }
-
-  return new TextDecoder().decode(bytes.buffer);
-}
+export interface Env {}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    // the path looks like /image/<payload>/og.png
-    const payload = request.url.split("/")[4];
+		const cover = new URL(request.url).searchParams.get('cover');
+		if (!cover) {
+			return new Response('Missing cover', { status: 400 });
+		}
 
-    if (!payload) {
-      return new Response("Missing payload", { status: 400 });
-    }
-
-    const decodedBase64 = decodeBase64Url(payload);
-    const decoded = JSON.parse(decodedBase64);
-    const { title, domain = "www.skorfmann.com" } = decoded;
-    return new ImageResponse(template(title, domain), {
-      format: "png",
-      width: 1200,
-      height: 627,
-    });
-  }
+		return new ImageResponse(template(cover), {
+			format: 'png',
+			width: 1200,
+			height: 630,
+		});
+	},
 };
